1df0ac1671313acf672853bedafa9df7be0f24d9
sajiiiid <elhaddadysajid@gmail.com>
2025-10-20 12:32:14 +0100

    Update ICoop.java

--- iccoop/src/main/java/ch/epfl/cs107/icoop/ICoop.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/ICoop.java
@@ -13,7 +13,7 @@ import ch.epfl.cs107.play.window.Window;
 
 
 public class ICoop extends AreaGame {
-    private final String[] areas = {"Spawn", "Orbway"};
+    private final String[] areas = {"Spawn", "OrbWay"};
     private ICoopPlayer player;
     private int areaIndex;
     private void createAreas() {
@@ -25,17 +25,18 @@ public class ICoop extends AreaGame {
         if (super.begin(window, fileSystem)) {
             createAreas();
             areaIndex = 0;
-            initArea(areas[areaIndex]);
+            setCurrentArea("Spawn", true);
+            //initArea(areas[areaIndex]); // this is what was originally here
             return true;
         }
         return false;
-    }
+    }/*
     @Override
     public void update(float deltaTime) {
         if (player.isWeak())
             switchArea();
         super.update(deltaTime);
-    }
+    }*/
     @Override
     public void end() {
 
@@ -44,18 +45,19 @@ public class ICoop extends AreaGame {
     public String getTitle() {
         return "ICoop";
     }
+    /*
     private void initArea(String areaKey) {
-        Tuto2Area area = (Tuto2Area) setCurrentArea(areaKey, true);
+        ICoopArea area = (ICoopArea) setCurrentArea(areaKey, true);
         DiscreteCoordinates coords = area.getPlayerSpawnPosition();
-        player = new GhostPlayer(area, Orientation.DOWN, coords, "ghost.1");
+        player = new ICoopPlayer(area, Orientation.DOWN, coords, "ghost.1");
         player.enterArea(area, coords);
         player.centerCamera();
     }
     private void switchArea() {
         player.leaveArea();
         areaIndex = (areaIndex == 0) ? 1 : 0;
-        Tuto2Area currentArea = (Tuto2Area) setCurrentArea(areas[areaIndex], false);
+        ICoopArea currentArea = (ICoopArea) setCurrentArea(areas[areaIndex], false);
         player.enterArea(currentArea, currentArea.getPlayerSpawnPosition());
         player.strengthen();
-    }
+    }*/
 }
