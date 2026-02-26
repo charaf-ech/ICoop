8c14a78abbdc6ae8e2de2b99466b91c88b466810
charaf-ech <cechchorfi@gmail.com>
2026-02-26 09:00:04 +0000

    Implement ICoopCollectable as an abstract class extending CollectableAreaEntity

--- iccoop/src/main/java/ch/epfl/cs107/icoop/actor/ICoopCollectable.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/ICoopCollectable.java
@@ -1,4 +1,35 @@
 package ch.epfl.cs107.icoop.actor;
 
-public class ICoopCollectable {
+import ch.epfl.cs107.play.areagame.actor.CollectableAreaEntity;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+
+public abstract class ICoopCollectable extends CollectableAreaEntity {
+
+    public ICoopCollectable(Area area, Orientation orientation, DiscreteCoordinates position) {
+        super(area, orientation, position);
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return false; // Traversable
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return false; // Par défaut, interaction de contact uniquement
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+        // La gestion concrète se fera dans les sous-classes ou via le visiteur
+        // Pour l'instant, on peut laisser le comportement par défaut ou délégue
+    }
 }
